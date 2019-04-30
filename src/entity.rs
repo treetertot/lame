0361eae5580a57e81c25f4bd5dd24d784d503aae
treetertot <europajuice@gmail.com>
2019-04-29 21:12:03 -0700

    added generic hitbox

--- src/entity.rs
+++ src/entity.rs
@@ -1,6 +1,15 @@
 /// The core of lame
 /// update uses index and world to allow more interaction
-pub trait Entity: Clone {
-    fn update(&self) -> Self;
+pub trait Entity<T, H: Hitbox<T>>: Sized {
+    fn update(&self, collision_data: T) -> Self;
     fn center(&self) -> (f32, f32);
+    fn get_hitbox(&self) -> &H;
+}
+
+pub trait Hitbox<T> {
+    fn resolve(&self, other: &Self) -> T;
+}
+
+pub fn run<T, H: Hitbox<T>, E: Entity<T, H>>(index: usize, world: &Vec<E>) {
+    
 }
