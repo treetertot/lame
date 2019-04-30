626087fbe588ff9ac3293aa5736f3293107afc6b
treetertot <europajuice@gmail.com>
2019-04-30 11:49:50 -0700

    made entity send and sync

--- src/entity.rs
+++ src/entity.rs
@@ -1,6 +1,6 @@
 /// The core of lame
 /// update uses index and world to allow more interaction
-pub trait Entity<T, H: Hitbox<T>>: Sized {
+pub trait Entity<T, H: Hitbox<T>>: Sized + Send + Sync {
     fn update(&self, collision_data: T) -> Self;
     fn center(&self) -> (f32, f32);
     fn get_hitbox(&self) -> &H;
