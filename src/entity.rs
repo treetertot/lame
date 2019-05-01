22bb2432162b76c2b233d4737e9692ffc16d28da
treetertot <europajuice@gmail.com>
2019-05-01 16:49:54 -0700

    i dun goofed

--- src/entity.rs
+++ src/entity.rs
@@ -1,30 +1,7 @@
-use std::sync::RwLockReadGuard;
+use crate::world::World;
 /// The core of lame
 /// update uses index and world to allow more interaction
-pub trait Entity<T: Sized + Send + Sync>: Sized + Send + Sync {
-    fn update(&self, collision_data: Option<T>) -> Self;
+pub trait Entity: Sized + Send + Sync {
+    fn update(&self, world: &World<Self>) -> Self;
     fn center(&self) -> (f32, f32);
-    fn resolve(&self, other: &Self) -> Option<T>;
-}
-
-pub fn run<T: Sized + Send + Sync, E: Entity<T>>(index: usize, world: RwLockReadGuard<Vec<E>>) -> E {
-    let entity = &world[index];
-    let mut it = world.iter().enumerate();
-    let resolution = loop {
-        match it.next() {
-            Some((i, other)) => if i == index {
-                if i == index {
-                    continue;
-                }
-                match entity.resolve(other) {
-                    Some(collision) => {
-                        break Some(collision);
-                    },
-                    None => (),
-                }
-            },
-            None => break None,
-        }
-    };
-    entity.update(resolution)
 }
--- src/world.rs
+++ src/world.rs
@@ -1,16 +1,19 @@
-use std::sync::RwLock;
+use std::sync::{RwLock, RwLockReadGuard};
 
 use crate::entity::{Entity};
 
-pub struct World<E: Entity<T>, T: Sized + Send + Sync> {
+pub struct World<E: Entity> {
     entities: RwLock<Vec<RwLock<E>>>,
-    _placeholder: T,
 }
 
-impl <E: Entity<T>, T: Sized + Send + Sync> World<E, T> {
-    pub fn update_offset(&self, offset_start: usize, offset_amount: usize) {
-        for i in (offset_start..self.entities.read().unwrap().len()).step_by(offset_amount) {
-
-        }
+impl<E: Entity> World<E> {
+    pub fn push(&self, entity: E) {
+        self.entities.write().unwrap().push(RwLock::new(entity));
+    }
+    pub fn kill(&self, number: usize) {
+        self.entities.write().unwrap().remove(number);
+    }
+    pub fn read_enemy(&self, number: usize) -> RwLockReadGuard {
+        self.entities.read().unwrap()
     }
 }
