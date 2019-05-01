c46c686fb08c04288c35f14353861dd1678d86eb
treetertot <europajuice@gmail.com>
2019-05-01 07:40:41 -0700

    finished run function

--- src/entity.rs
+++ src/entity.rs
@@ -6,15 +6,24 @@ pub trait Entity<T: Sized + Send + Sync>: Sized + Send + Sync {
     fn resolve(&self, other: &Self) -> Option<T>;
 }
 
-pub fn run<E: Entity>(index: usize, world: &Vec<E>) {
+pub fn run<T: Sized + Send + Sync, E: Entity<T>>(index: usize, world: &Vec<E>) -> E {
     let entity = &world[index];
     let mut it = world.iter().enumerate();
     let resolution = loop {
         match it.next() {
             Some((i, other)) => if i == index {
-                
+                if i == index {
+                    continue;
+                }
+                match entity.resolve(other) {
+                    Some(collision) => {
+                        break Some(collision);
+                    },
+                    None => (),
+                }
             },
             None => break None,
         }
-    }
+    };
+    entity.update(resolution)
 }
