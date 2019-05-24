d670149f5c7f63a99202fcb9055a132fe7588b75
treetertot <europajuice@gmail.com>
2019-05-24 14:50:19 -0700

    Removed center method. Why was that even there?

--- src/entity.rs
+++ src/entity.rs
@@ -3,5 +3,4 @@ use crate::world::World;
 /// update uses index and world to allow more interaction
 pub trait Entity: 'static + Sized + Send + Sync + Clone {
     fn update(entity_num: usize, world: &World<Self>);
-    fn center(&self) -> (f32, f32);
 }
