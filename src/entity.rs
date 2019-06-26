03f26fbb06f6273b1cd37c22f6760a9d3039c41a
treetertot <europajuice@gmail.com>
2019-06-25 17:33:36 -0700

    removed an unnecessary static

--- src/entity.rs
+++ src/entity.rs
@@ -1,4 +1,4 @@
 use crate::ops::Ops;
-pub trait Entity<S: 'static + Send + Sync + Sized>: 'static + Send + Sync + Sized {
+pub trait Entity<S: 'static + Send + Sync + Sized>: Send + Sync + Sized {
     fn update(&mut self, shared: &S, delta: f32) -> Option<Vec<Ops<Self>>>;
 }
