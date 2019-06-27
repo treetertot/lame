acf485acbab5dd0469cf2cf6175f83475d0c6395
treetertot <europajuice@gmail.com>
2019-06-27 14:17:33 -0700

    removed a stupid requirement

--- src/entity.rs
+++ src/entity.rs
@@ -1,4 +1,4 @@
 use crate::ops::Ops;
-pub trait Entity<S: 'static + Send + Sync + Sized>: Send + Sync + Sized {
+pub trait Entity<S: 'static + Send + Sync + Sized>: Send + Sized {
     fn update(&mut self, shared: &S, delta: f32) -> Option<Vec<Ops<Self>>>;
 }
